4b075fa44fdf739cc5bbc19bb82befd09efba7b4
infinity390 <75435931+infinity390@users.noreply.github.com>
2025-12-19 08:37:34 +0530

    Update __init__.py

--- chemistryai/__init__.py
+++ chemistryai/__init__.py
@@ -1,8 +1,1 @@
-from .chemlogic import *
-
-def iupac(graph):
-    return tree_to_iupac(build_tree_recursive(graph))
-def smiles(string):
-    return smiles_to_graphnode(string)
-def draw(graph, filename="compound.png", size=(600, 400)):
-    draw_graph_with_rdkit(graph, filename, size)
+from .chemlogic import iupac, smiles, draw
