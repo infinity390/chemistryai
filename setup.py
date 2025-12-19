74f256a47c72514c73851623c0c7df55c0c3cdf6
infinity390 <75435931+infinity390@users.noreply.github.com>
2025-12-19 08:38:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chemistryai",
-    version="0.1.0",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=[
         "rdkit-pypi",
@@ -20,3 +20,4 @@ setup(
         "Operating System :: OS Independent",
     ],
 )
+
